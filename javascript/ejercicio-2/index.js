6a25f7e65022e9f87d7bfe0c8d5014b1557a4ba4
rodrigoWSL <ingenieria.rlopez@gmail.com>
2022-09-19 15:46:27 -0300

    item list

---
+++ javascript/ejercicio-2/index.js
@@ -0,0 +1,14 @@
+const lista = [
+    'Rodrigo', 
+    32, 
+    true, 
+    new Date("sep 04 1990"),
+    libro = {
+        titulo: "El amor en los tiempos del cólera",
+        autor: "Gabriel Garcia Marquez",
+        fecha: 1995,
+        url: 'https://es.wikipedia.org/wiki/El_amor_en_los_tiempos_del_c%C3%B3lera'
+    }
+ ]
+
+console.log(lista);
